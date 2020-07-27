28990831e2a74f98fa8819f1b8ed920756f24671
AngeHCobos <cobos.haydee@gmail.com>
2020-07-26 23:25:42 -0300

    funcion get

--- request.py
+++ request.py
@@ -1,1 +1,8 @@
 import requests
+
+APIKEY = "qajYO5e4OdI1o95E1elfg6C6QvJ4egI6WeZ4QTa6"
+
+def get(endpoint):
+    return requests.get('https://itch.io/api/1/' + APIKEY + endpoint)
+
+print(get("/games").text)
